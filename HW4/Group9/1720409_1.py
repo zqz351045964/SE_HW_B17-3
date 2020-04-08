25971883177f9046cc83e17ecc35e4188d9e5c95
liweiwei620 <1617980763@qq.com>
2020-04-08 21:08:41 +0800

    first

---
+++ HW4/Group9/1720409_1.py
@@ -0,0 +1,7 @@
+a=0
+for x in range(1,20):
+     for y in range(1,34):
+          for z in range(1,100):
+               if (5*x+3*y+z/3==100) and x+y+z==100:
+                 a=a+1;
+                 print("买法:",a,"公鸡:",x,"母鸡:",y,"小鸡:",z)
---
+++ HW4/Group9/1720409_2.py
@@ -0,0 +1,7 @@
+a = 30
+b = 90
+for x in range(1, a):
+    y = a - x
+    if 2 * x + 4 * y == b:
+        print("鸡有", x,  "只。")
+        print("兔有", y,  "只。")
