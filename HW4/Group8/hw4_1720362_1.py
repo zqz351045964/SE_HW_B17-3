dacf708a0f6ce4e992a211fe3eb7baa7ef7ec885
1720362 <61856154+kingsley-ky@users.noreply.github.com>
2020-04-08 23:19:59 +0800

    Update hw4_1720362_1.py

--- HW4/Group8/hw4_1720362_1.py
+++ HW4/Group8/hw4_1720362_1.py
@@ -1,5 +1,5 @@
 for x in range(21):                                                         # 公鸡定义变量为x，且最多为20只
- for y in range(34):                                                        # 母鸡定义变量为y，且最多为33只
+   for y in range(34):                                                        # 母鸡定义变量为y，且最多为33只
          z = 100-x-y
- if (z%3==0 and 5*x + 3*y + z/3 == 100):                                     # 总共花费100元
- print(x,y,z)
+         if (z%3==0 and 5*x + 3*y + z/3 == 100):                                     # 总共花费100元
+            print(x,y,z)
