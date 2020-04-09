4c5208ae3ee34ff4ba76ef9092b1cf737b42db77
newhereswc <61897019+newhereswc@users.noreply.github.com>
2020-04-09 16:07:49 +0800

    Add files via upload

---
+++ HW4/Group14/hw4_1720423_1.py
@@ -0,0 +1,8 @@
+flag = 0
+for i in range(0, 20):
+    for j in range(0, 33):
+        k = 100 - i - j
+        if (k % 3 == 0) and (5 * i + 3 * j + k / 3 == 100):
+            print('公鸡：%s 母鸡：%s 小鸡：%s' % (i, j, k))
+            flag = flag + 1
+print('共有%d种方法' % flag)
