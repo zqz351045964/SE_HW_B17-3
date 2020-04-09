a2c132b4a48f3ae21242bd1975e5e234d989073c
xxy624 <12360906@qq.com>
2020-04-09 12:18:22 +0800

    作业4

--- HW4/Group11/hw4_1720416_1.py
+++ HW4/Group11/hw4_1720416_1.py
@@ -1,10 +1,6 @@
 //公鸡最多可买20只，母鸡最多可买33只。公鸡x，母鸡y，小鸡z。
-count = 0
 for x in range(0, 20):
     for y in range(0, 33):
         z = 100-x-y
         if (z%3 == 0) & (5*x + 3*y + z/3 == 100):
-            print('公鸡：%s 只，母鸡：%s 只，小鸡：%s 只' %(x,y,z))
-            count = count+1
-print('---')
-print('共有 %d 种买法' % count)
+            print(x,y,z)
--- HW4/Group11/hw4_1720416_2.py
+++ HW4/Group11/hw4_1720416_2.py
@@ -1,5 +1,4 @@
-//共90只脚，都是鸡45只，都是兔23只
-for x in range(0, 45):
-    for y in range(0, 23):
-        if (x + y == 30) and (x*2 + y*4 == 90):
-            print('鸡：%s 只，兔：%s 只' % (x, y))
+//共90只脚，都是鸡45只，都是兔23只。鸡兔一共30只。鸡x只，兔y只。
+for x in range(0, 30):
+    if (x + y == 30) and (x*2 + y*4 == 90):
+        print(x, y)
